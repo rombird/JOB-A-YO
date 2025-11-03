f6463d6924bb0e0b984fb9adee901c0ad0f5120d
rombird <romsae5510@gmail.com>
2025-11-03 17:51:39 +0900

    DB 작업하기

---
+++ JOB-A-YO/Back/src/main/java/com/example/demo/dto/UserDto.java
@@ -0,0 +1,18 @@
+package com.example.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+    private String username;
+
+    private String password1;
+
+    private String password2;
+
+    private String email;
+}
