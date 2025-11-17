aed1c409ec43f7276b90036b97d6924124359f19
ddaeng2001 <ddaeng2001@naver.com>
2025-11-17 17:50:48 +0900

    notices_entity부분부터

--- JOB-A-YO/Back/src/main/java/com/example/demo/domain/entity/NoticesEntity.java
+++ JOB-A-YO/Back/src/main/java/com/example/demo/domain/entity/NoticesEntity.java
@@ -1,8 +1,7 @@
 package com.example.demo.domain.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,5 +13,22 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "notices_table")
-public class NoticesEntity {
-}
+public class NoticesEntity extends BaseEntity { //RDBMS TB 표현, JPA가 이 클래스를 테이블로 인식하여 관리가능
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String author;
+
+    @Column(nullable=false)
+    private String noticesTitle;
+
+    @Lob //긴 텍스트 저장
+    @Column(nullable=false)
+    private String noticesContents;
+
+
+
+
+    }
